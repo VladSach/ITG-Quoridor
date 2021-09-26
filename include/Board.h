1df846b052d3cd4cb7b553656f575f8dbc193c2b
VladSach <vladsach@gmail.com>
2021-09-26 20:23:07 +0300

    Here we go again

--- include/Board.h
+++ include/Board.h
@@ -1,11 +1,12 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include "Tile.h"
 #include "Player.h"
 
-enum {
-    BoardSize = 9
+enum Field {
+    empty = 0,
+    tile  = 1,
+    wall  = 2
 };
 
 enum PlayersChoice {
@@ -13,9 +14,21 @@ enum PlayersChoice {
     changePosition
 };
 
+enum Walls { 
+    leftWall  = 0x01,  // 0000 0001 
+    upperWall = 0x02,  // 0000 0010
+    lowerWall = 0x04,  // 0000 0100
+    rightWall = 0x08,  // 0000 1000
+    //lowMask = 0xFF // bitmask for storing last 4 bits
+};
+
 class Board {
 private:
-    Tile map[BoardSize][BoardSize]; 
+    char **map; 
+    const int m_mapSize = 17;
+
+    // m_walls is a bitset
+    unsigned char m_walls = 0x0; // 1 byte
 
     Player firstPlayer;
     Player secondPlayer;
@@ -27,6 +40,7 @@ public:
     ~Board() = default;
 
     void initBoard();
+    void setWall(unsigned char wall);
     void playerMove(Player *currentPlayer, PlayersChoice choice);
     
 };
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,15 +1,34 @@
 #include "Board.h"
 
 void Board::initBoard() {
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            map[i][j].setPosition(i, j);
-        }
-    }
+    map = new char*[m_mapSize]{
+        "01010101010101010",
+        "11111111111111111",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010", 
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+        "01010101010101010",
+    };
 
     // TODO: do this in a random way
     currentPlayer = &firstPlayer;
 
+}
+void Board::setWall(unsigned char wall) {
+    // todo: add check for ability to place wall
+    // todo: add check if player has any walls
+    m_walls |= wall;
 }
 
 void Board::playerMove(Player *currentPlayer, PlayersChoice choice) {
@@ -26,3 +45,22 @@ void Board::playerMove(Player *currentPlayer, PlayersChoice choice) {
         break;
     }
 }
+
+// bool Tile::getWall(unsigned char wall) {
+//     switch (wall) {
+//     case leftWall:
+//         return (m_Walls & leftWall) ? true : false;
+
+//     case upperWall:
+//         return (m_Walls & upperWall) ? true : false;
+
+//     case lowerWall:
+//         return (m_Walls & lowerWall) ? true : false;
+
+//     case rightWall:
+//         return (m_Walls & rightWall) ? true : false;
+    
+//     default:
+//         return false;
+//     }
+// }
