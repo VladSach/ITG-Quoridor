767bbed72bd95a42335fc09a5a406ccf84a83b3a
Yurii <Yurri2002@gmail.com>
2021-10-01 02:30:06 +0300

    strange code

--- include/View.h
+++ include/View.h
@@ -3,10 +3,20 @@
 
 #include "Game.h"
 #include "Observer.h"
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+const int SIZE = 9;
+const int FIELD_SIZE = 530;
+const int CELL_SIZE = 50;
+const int ARRAY_SIZE = 17;
 
 class View : public Observer {
 private:
     Game *m_model;
+    sf::Texture mTexture;
+    sf::Sprite mSprite;
 
 public:
     View(Game *model);
--- src/View.cpp
+++ src/View.cpp
@@ -1,8 +1,16 @@
 #include "View.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
 
 View::View(Game *model) {
     m_model = model;
     m_model->addObserver(this);
+
+    mTexture.loadFromFile("./media/map.png");
+    mTexture.setSmooth(true);
+    mSprite.setTexture(mTexture);
 }
 
 void View::update() {
@@ -17,57 +25,114 @@ void View::update() {
     drawMap(m_model->getBoard());
 }
 
+
+// void View::draw(sf::RenderTarget& target, sf::RenderStates states) const
+// {
+//     states.transform *= getTransform();
+//     sf::Color color = sf::Color(200, 100, 200);
+
+//     sf::RectangleShape shape(sf::Vector2f(FIELD_SIZE, FIELD_SIZE));
+//     shape.setOutlineThickness(2.f);
+//     shape.setOutlineColor(color);
+//     spahe.setFillColor(sf::Color::Transparent);
+    
+//     for (unsigned int i = 0; i < ARRAY_SIZE; i++);{
+//         sf::Vector2f position(i % SIZE * CELL_SIZE + 10.f, i / SIZE * CELL_SIZE + 10.f);
+//         shape.setPosition(position);
+//         target.draw(shape, states);
+//     }
+// }
+
 void View::drawMap(Board board) {
     int x1, y1, x2, y2;
-    m_model->getFirstPlayerPosition(&x1, &y1);
+    m_model->getFirstPlayerPosition(&x1, &y1);  
     m_model->getSecondPlayerPosition(&x2, &y2);
 
-    std::cout << "    ";
-    for (int i = 0; i < mapSize; i++) {
-        if (i < 10)
-        std::cout << i << ' ';
-        else if (i >= 10) 
-        std::cout << i-10 << ' ';
-    }
-    std::cout << std::endl;
-    
-    std::cout << "    ";
-    for (int i = 0; i < mapSize; i++) {
-        std::cout << '-' << ' ';
-    }
-    std::cout << std::endl;
+    // sf::String TileMap[] = 
 
-    for (int i = 0; i < mapSize; i++) {
-        if (i < 10)
-        std::cout << i << ' ' << '|' << ' ';
-        else if (i >= 10) 
-        std::cout << i << '|' << ' ';
+    sf::RenderWindow window(sf::VideoMode({510, 510}), "Quoridor");
 
-            for (int j = 0; j < mapSize; j++) {
-                if (x1 == j && y1 == i) {
-                    std::cout << 'P' << ' ';
-                } else if (x2 == j && y2 == i) {
-                    std::cout << 'S' << ' ';
-                } else {
-                    int c = board.getTile(j, i);
-                    if (c == 0) std::cout << ' ';
-                    else if (c == 1) std::cout << '.';
-                    else if (c == 2) std::cout << '#';
-                    std::cout << ' ';
-                }
+    while (window.isOpen()){
+
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
             }
-        std::cout << std::endl;
+            if (event.type == sf::Event::MouseButtonReleased){}
+        }
+
+        window.clear(sf::Color(0x80, 0x80, 0x0));
+
+        for(float i = 0; i < ARRAY_SIZE; i++){
+            for(float j = 0; j < ARRAY_SIZE; j++){
+                switch (board.getTile(i,j))
+                {
+                case 1:
+                    mSprite.setTextureRect(sf::IntRect(50, 0, 30, 30));
+                    break;
+                
+                case 0:
+                    mSprite.setTextureRect(sf::IntRect(0, 0, 30, 30));
+                    break;
+                }
+                // sf::RectangleShape shape1(sf::Vector2f(50, 50));
+                // shape1.setPosition({i * 70, j * 70});
+                // shape1.setFillColor(sf::Color(0x80, 0x0, 0x0));
+                // window.draw(shape1);
+                mSprite.setPosition(j * 30, i * 30);
+                window.draw(mSprite);
+            }
+        }
+        window.display();
     }
-    std::cout << std::endl;
 
-    std::vector<std::pair<int, int>> moves = m_model->getPossibleMoves();
+    // std::cout << "    ";
+    // for (int i = 0; i < mapSize; i++) {
+    //     if (i < 10)
+    //     std::cout << i << ' ';
+    //     else if (i >= 10) 
+    //     std::cout << i-10 << ' ';
+    // }
+    // std::cout << std::endl;
+    
+    // std::cout << "    ";
+    // for (int i = 0; i < mapSize; i++) {
+    //     std::cout << '-' << ' ';
+    // }
+    // std::cout << std::endl;
 
-    std::cout << m_model->getCurrentPlayerName() << " Can move as this: ";
-    for(auto e : moves) {
-        std::cout << e.first << ' ' << e.second << ' ';
-        std::cout << '|' << ' ';
-    }
+    // for (int i = 0; i < mapSize; i++) {
+    //     if (i < 10)
+    //     std::cout << i << ' ' << '|' << ' ';
+    //     else if (i >= 10) 
+    //     std::cout << i << '|' << ' ';
 
-    std::cout << std::endl;
-    std::cout << std::endl;
+    //         for (int j = 0; j < mapSize; j++) {
+    //             if (x1 == j && y1 == i) {
+    //                 std::cout << 'P' << ' ';
+    //             } else if (x2 == j && y2 == i) {
+    //                 std::cout << 'S' << ' ';
+    //             } else {
+    //                 int c = board.getTile(j, i);
+    //                 if (c == 0) std::cout << ' ';
+    //                 else if (c == 1) std::cout << '.';
+    //                 else if (c == 2) std::cout << '#';
+    //                 std::cout << ' ';
+    //             }
+    //         }
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+
+    // std::vector<std::pair<int, int>> moves = m_model->getPossibleMoves();
+
+    // std::cout << m_model->getCurrentPlayerName() << " Can move as this: ";
+    // for(auto e : moves) {
+    //     std::cout << e.first << ' ' << e.second << ' ';
+    //     std::cout << '|' << ' ';
+    // }
+
+    // std::cout << std::endl;
+    // std::cout << std::endl;
 }
