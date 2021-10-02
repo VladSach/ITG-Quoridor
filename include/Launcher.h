97de91ecbbc289b55269e0a9235aa65b08a89a27
Yurii <Yurri2002@gmail.com>
2021-10-02 16:18:02 +0300

    attempt to create launcher

---
+++ include/Launcher.h
@@ -0,0 +1,35 @@
+#ifndef LAUNCHER_H
+#define LAUNCHER_H
+
+#include "Observer.h"
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+
+
+class Launcher: public Observer {
+private:
+    sf::Texture singlePlayerTexture;
+    sf::Texture multiplayerTexture;
+    sf::Texture exitTexture;
+
+    sf::Sprite singePlayer;
+    sf::Sprite multiplayer;
+    sf::Sprite exit;
+
+    sf::RenderWindow window;
+
+    bool isMenu;
+    int menuNum;
+
+public:
+    Launcher();
+    ~Launcher() = default;
+
+    virtual void update();
+    
+    void menu();
+};
+
+#endif // LAUNCHER_H
