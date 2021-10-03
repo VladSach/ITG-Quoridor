29dc51c8cca4631cfcc0da1813b142f99cea74e3
Yurii <Yurri2002@gmail.com>
2021-10-04 00:24:58 +0300

    fix

--- src/View.cpp
+++ src/View.cpp
@@ -100,7 +100,7 @@ void View::drawMap(Board board) {
     }
 
     for (int i = 0; i < walls2; i++){
-        boardSprite.setTextureRect(sf::IntRect(0, 50, CELL_SIZE*3, CELL_SIZE));
+        boardSprite.setTextureRect(sf::IntRect(0, 50, CELL_SIZE, CELL_SIZE));
         boardSprite.setPosition(CELL_SIZE*mapSize + border + 10, 45 + i*45);
         m_Window.draw(boardSprite);
         boardSprite.setPosition(CELL_SIZE*mapSize + border + 10 + CELL_SIZE, 45 + i*45);
